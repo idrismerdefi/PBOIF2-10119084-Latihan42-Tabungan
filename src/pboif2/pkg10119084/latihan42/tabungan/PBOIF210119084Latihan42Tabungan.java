8c303dcb7240219fcf0a5bd3f5d6763bfdd14abb
idrismerdefi <67642938+idrismerdefi@users.noreply.github.com>
2020-11-13 11:01:09 +0700

    Update PBOIF210119084Latihan42Tabungan.java

--- src/pboif2/pkg10119084/latihan42/tabungan/PBOIF210119084Latihan42Tabungan.java
+++ src/pboif2/pkg10119084/latihan42/tabungan/PBOIF210119084Latihan42Tabungan.java
@@ -4,12 +4,12 @@
  * and open the template in the editor.
  */
 package pboif2.pkg10119084.latihan42.tabungan;
-
 /**
- *
- * @author MERDEFI
+Nama : Muhammad Idris Merdefi
+Kelas : PBOIF2
+NIM : 10119084
+Deskripsi Program : Program ini berisi program untuk menampilkan tabungan
  */
-
 
 import java.util.Scanner;
 public class PBOIF210119084Latihan42Tabungan {
